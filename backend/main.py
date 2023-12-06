a7c57beac4dd422d94a5fdb10d79649899032dbf
Kingomac <kingomacyt@gmail.com>
2023-12-06 22:43:25 +0100

    Configuración CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from .controllers import series,personality
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost"
+]
+
 router = APIRouter(prefix="/api")
 
 
@@ -14,4 +22,11 @@ async def main():
 router.include_router(personality.router)
 router.include_router(series.router)
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 app.mount("/static", StaticFiles(directory="backend/static"), name="static")
